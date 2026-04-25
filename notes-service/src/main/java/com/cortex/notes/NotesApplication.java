c65241a0a2f62e7a87222197b1d66306fa53a0bf
shanmukhraj7 <148477598+shanmukhraj7@users.noreply.github.com>
2026-04-26 00:44:38 +0530

    Updated Complete Notes-Service

--- notes-service/src/main/java/com/cortex/notes/NotesApplication.java
+++ notes-service/src/main/java/com/cortex/notes/NotesApplication.java
@@ -2,8 +2,10 @@ package com.cortex.notes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class NotesApplication {
 
     public static void main(String[] args) {
